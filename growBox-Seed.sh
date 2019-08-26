2655808c364bd61e1cfc0dc3c3cf94a051b8a92b
Michael Bogle <mbogle@mjns.it>
2019-08-25 23:34:28 -0400

    adding roles

--- growBox-Seed.sh
+++ growBox-Seed.sh
@@ -14,6 +14,7 @@ gbRootRepo='https://github.com/treestarsystems/growBox-Root.git'
 gbStemRepo='https://github.com/treestarsystems/growBox-Stem.git'
 gbBranchRepo='https://github.com/treestarsystems/growBox-Branch.git'
 gbFlowerRepo='https://github.com/treestarsystems/growBox-Flower.git'
+gbSoilRepo='https://github.com/treestarsystems/growBox-Soil.git'
 gbPetalRepo='https://github.com/treestarsystems/growBox-Petal.git'
 gbSeedLog="growBox-Seed.log"
 
@@ -35,7 +36,7 @@ function do_parse_project {
 	echo "${array[-1]%.*}"
 }
 
-# gen_user_name <project>
+#gen_user_name <project>
 function gen_user_name {
 	strip_dash="${1##*-}"
 	tolower=$(sed -e 's/\(.*\)/\L\1/' <<< "gb$strip_dash$(genrandom 5)")
@@ -45,13 +46,26 @@ function gen_user_name {
 function do_user_create {
 	if ["$user_name" == ''] || ["$password" == '']
 		then
-			user_name=gb$(sed -e 's/\(.*\)/\L\1/' <<<$(gen_user_name $project))
+			user_name=$(sed -e 's/\(.*\)/\L\1/' <<<$(gen_user_name $project))
 			password=$(genrandom 20)
 	fi
 	#Add user to system
 	useradd -d $gbSeedDir/$project -s /bin/bash -U $user_name
 	echo -e "$password\n$password" | passwd $user_name
 	chown -R $user_name:$user_name $gbSeedDir/$project
+	echo Username: $user_name
+	echo Password: $password
+}
+
+function do_user_create_only {
+	if ["$user_name" == ''] || ["$password" == '']
+		then
+			user_name=$(sed -e 's/\(.*\)/\L\1/' <<<$(gen_user_name $project))
+			password=$(genrandom 20)
+	fi
+	#Add user to system
+	useradd -s /bin/bash -U $user_name
+	echo -e "$password\n$password" | passwd $user_name
 	echo Username: $user_name
 	echo Password: $password
 }
@@ -65,11 +79,33 @@ function do_system_dependencies {
 	npm install pm2 -g
 }
 
-function do_config_edit {
+function do_system_dependencies_soil {
+	echo "Installing Soil system dependencies"
+	apt update
+	apt -y upgrade
+	apt-get install -y zfsutils-linux nfs-kernel-server ifenslave ifupdown resolvconf gdisk
+	rm /etc/netplan/01-netcfg.yaml
+}
+
+function do_system_dependencies_petal {
+	echo "Installing Petal system dependencies"
+	apt update
+	apt -y upgrade
+	apt install -y uv4l uv4l-raspicam uv4l-raspicam-extras uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-dummy uv4l-webrtc uv4l-xmpp-bridge
+}
+
+function do_config_edit_basic {
 	#Add config options to the /boot/config.txt
 	echo -e '\n#Temp Sensor Edit\ndtoverlay=w1-gpio\n\n#Disable Warning Overlays and allow turbo mode\navoid_warnings=2\n' >> /boot/config.txt
 }
 
+function do_config_edit_petal {
+	#Add config options to the /boot/config.txt
+	echo -e "\nstart_x=1\ngpu_mem=128\ndisable_camera_led=1" >> /boot/config.txt
+	sed -i "s/nopreview = no/nopreview = yes/g" /etc/uv4l/uv4l-raspicam.conf
+}
+
+#Needs a lot of work to get right but I will use the work done with postReceive.
 function do_pm2conf {
 	echo -e "" > /root/ecosystem.config.js
 }
@@ -89,79 +125,94 @@ function do_root {
 	cd $gbSeedDir/$project
 	npm install
 	do_user_create
-	do_pm2conf
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+	#Needs a lot of work to get right but I will use the work done with postReceive.
+#	do_pm2conf
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
 }
 
 function do_stem {
 	project=$(do_parse_project $gbStemRepo)
 	echo "Installing Stem Role"
 	do_system_dependencies
-	do_config_edit
+	do_config_edit_basic
 	mkdir $gbSeedDir
 	cd $gbSeedDir
 	git clone $gbStemRepo
 	cd $gbSeedDir/$project
 	npm install
 	do_user_create
-	do_pm2conf
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+	#Needs a lot of work to get right but I will use the work done with postReceive.
+#	do_pm2conf
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
 }
 
 function do_branch {
 	project=$(do_parse_project $gbBranchRepo)
 	echo "Installing Branch Role"
 	do_system_dependencies
-	do_config_edit
+	do_config_edit_basic
 	mkdir $gbSeedDir
 	cd $gbSeedDir
 	git clone $gbBranchRepo
 	cd $gbSeedDir/$project
 	npm install
 	do_user_create
-	do_pm2conf
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+	#Needs a lot of work to get right but I will use the work done with postReceive.
+#	do_pm2conf
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
 }
 
 function do_flower {
 	project=$(do_parse_project $gbFlowerRepo)
 	echo "Installing Flower Role"
 	do_system_dependencies
-	do_config_edit
+	do_config_edit_basic
 	mkdir $gbSeedDir
 	cd $gbSeedDir
 	git clone $gbFlowerRepo
 	cd $gbSeedDir/$project
 	npm install
 	do_user_create
-	do_pm2conf
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+	#Needs a lot of work to get right but I will use the work done with postReceive.
+#	do_pm2conf
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
+}
+
+function do_soil {
+	project=$(do_parse_project $gbSoilRepo)
+	echo "Installing Soil Role"
+	do_system_dependencies
+	do_system_dependencies_soil
 }
 
 function do_petal {
 	project=$(do_parse_project $gbPetalRepo)
 	echo "Installing Petal Role"
+	do_config_edit_basic
+	curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
+	echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" >> /etc/apt/sources.list
 	do_system_dependencies
-	do_config_edit
+	do_config_edit_petal
 	mkdir $gbSeedDir
 	cd $gbSeedDir
 	git clone $gbPetalRepo
 	cd $gbSeedDir/$project
 	npm install
 	do_user_create
-	do_pm2conf
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+	#Needs a lot of work to get right but I will use the work done with postReceive.
+#	do_pm2conf
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
 }
 
 function do_all {
 	echo "Installing All Roles"
 	do_system_dependencies
-	array=($gbRootRepo $gbStemRepo $gbBranchRepo $gbFlowerRepo)
+	array=($gbRootRepo $gbStemRepo $gbBranchRepo $gbFlowerRepo $gbSoil $gbPetal)
 	for i in "${array[@]}";
         do
 		project=$(do_parse_project $i)
@@ -171,9 +222,13 @@ function do_all {
 		git clone $i
 		cd $gbSeedDir/$project
 		npm install
-		if ["$project" != 'growBox-Root']
+		if ["$project" != 'growBox-Root'] || ["$project" != 'growBox-Soil']
 			then
-				do_config_edit
+				do_config_edit_basic
+		fi
+		if ["$project" == 'growBox-Petal']
+			then
+				do_config_edit_petal
 		fi
 		do_pm2conf
         done
@@ -191,8 +246,8 @@ function do_all {
 
 	echo Username: $user_name
 	echo Password: $password
-	pm2 /root/ecosystem.config.js
-	pm2 startup
+#	pm2 /root/ecosystem.config.js
+#	pm2 startup
 }
 
 function do_help {
@@ -210,6 +265,7 @@ function do_help {
         echo "  -s <username> <password-for-user>: Install growBox-Stem role"
         echo "  -b <username> <password-for-user>: Install growBox-Branch role"
         echo "  -f <username> <password-for-user>: Install growBox-Flower role"
+        echo "  -d <username> <password-for-user>: Install growBox-Soil role"
         echo "  -p <username> <password-for-user>: Install growBox-Petal role"
         echo "  -a <username> <password-for-user>: Install all growBox roles"
         echo ""
@@ -241,6 +297,10 @@ case $secondary in
 	-f )
 		echo -e "\n Install directory: $gbSeedDir \n Log file location: ~/$gbSeedLog-flower\n\n **Check log file for credentials** \n PLEASE DELETE WHEN DONE!!!\n rm ~/$gbSeedLog-flower\n"
 		do_flower $user_name $password > ~/$gbSeedLog-flower 2>&1
+ 		;;
+	-d )
+		echo -e "\n Install directory: $gbSeedDir \n Log file location: ~/$gbSeedLog-soil\n\n **Check log file for credentials** \n PLEASE DELETE WHEN DONE!!!\n rm ~/$gbSeedLog-soil\n"
+		do_soil $user_name $password > ~/$gbSeedLog-soil 2>&1
  		;;
 	-p )
 		echo -e "\n Install directory: $gbSeedDir \n Log file location: ~/$gbSeedLog-petal\n\n **Check log file for credentials** \n PLEASE DELETE WHEN DONE!!!\n rm ~/$gbSeedLog-petal\n"
@@ -290,7 +350,7 @@ case $primary in
 		secondary=$1; shift
 		user_name=$1
 		password=$2
-		do_user_create $user_name $password > ~/$gbSeedLog-adduser 2>&1
+		do_user_create_only $user_name $password > ~/$gbSeedLog-adduser 2>&1
 
 		shift $((OPTIND -1))
 		;;
